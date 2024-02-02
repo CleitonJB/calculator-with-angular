e3a41d1ec7e06f2acdd5a5a3eeb3f925dc0ebc88
Cleiton Braga <cleitonbraga56@gmail.com>
2024-02-02 20:36:14 -0300

    Create base routes of app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: 'calculator'
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'calculator',
+  },
+  {
+    path: 'calculator',
+    loadChildren: () => import('./pages/calculator/calculator.module').then(m => m.CalculatorModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
