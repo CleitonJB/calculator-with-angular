5dc9de45d361c83eceed0659e147a3dfc2b6ac2f
Cleiton Braga <cleitonbraga56@gmail.com>
2024-02-14 01:30:23 -0300

    Update calculator logic with mapped keys

--- src/app/pages/calculator/calculator.component.ts
+++ src/app/pages/calculator/calculator.component.ts
@@ -8,71 +8,112 @@ import { KeyTypes } from 'src/app/models/keyTypes';
   styleUrls: ['./calculator.component.scss']
 })
 export class CalculatorComponent implements OnInit {
-  public KEY_TYPES = KeyTypes;
+  public KEY_TYPES: typeof KeyTypes = KeyTypes;
 
   constructor() {}
   
-  ngOnInit(): void {
-    //this.setToVisor("Bem-vindo!", KeyTypes.symbol);
-  }
+  ngOnInit(): void {}
 
   public keyPressed(value: string, type: KeyTypes): void {
-    this.setToVisor(value, type);
+    //console.log(value.target.innerText);
+    this.handleKeyPressed(value, type);
   }
 
-  private setToVisor(value: string, type: KeyTypes): void {
+  private handleKeyPressed(value: string, type: KeyTypes): void {
+    switch(type) {
+      case KeyTypes.action:
+        const allowedActions: any = {
+          "clear": () => {
+            this.clearVisor();
+          },
+          "=": () => {
+            //*regras
+            const visorValue: string = this.getVisorElement()!.value;
+
+            if(visorValue.length > 0) {
+              const expressionResult: string = this.calculate();
+              this.clearVisor();
+              this.setToVisor(expressionResult);
+            } else {
+              this.setToVisor("0");
+            }
+          },
+        };
+
+        allowedActions[value]();
+      break;
+
+      case KeyTypes.operation:
+        const allowedOperations: any = {
+          "+": () => { return this.setToVisor("+"); },
+          "-": () => { return this.setToVisor("-"); },
+          "x": () => { return this.setToVisor("*"); },
+          "÷": () => { return this.setToVisor("/"); }
+        };
+
+        allowedOperations[value]();
+      break;
+
+      case KeyTypes.symbol:
+        const allowedSymbols: any = {
+          ".": () => { return this.setToVisor("."); },
+          ",": () => { return this.setToVisor(","); },
+          "parenthesis_open": () => { return this.setToVisor("(");},
+          "parenthesis_close": () => { return this.setToVisor(")"); }
+        };
+
+        allowedSymbols[value]();
+      break;
+
+      case KeyTypes.numeral:
+        const allowedNumerals: any = {
+          0: () => { return this.setToVisor("0"); },
+          1: () => { return this.setToVisor("1"); },
+          2: () => { return this.setToVisor("2"); },
+          3: () => { return this.setToVisor("3"); },
+          4: () => { return this.setToVisor("4"); },
+          5: () => { return this.setToVisor("5"); },
+          6: () => { return this.setToVisor("6"); },
+          7: () => { return this.setToVisor("7"); },
+          8: () => { return this.setToVisor("8"); },
+          9: () => { return this.setToVisor("9"); }
+        };
+
+        allowedNumerals[value]();
+      break;
+    }
+  }
+
+  private clearVisor(): void {
+    this.getVisorElement()!.value = '';
+  }
+
+  private calculate(): string {
     try {
-      const visorElement: HTMLElement | null = document.getElementById('calculator-visor'); // HTMLInputElement
+      const visorValue: string = this.getVisorElement()!.value; // expression
+      return (eval(visorValue) || eval(visorValue) == 0) ? eval(visorValue) as string : 'ERROR'; // Com o tratamento do try/catch, esse treco deve ficar irrelevante
+    } catch(error: any) {
+      return 'Erro de sintaxe :/';
+    }
+  }
 
-      if(visorElement) {
-        switch(type) {
-          case KeyTypes.action:
-            switch(value) {
-              case 'clear':
-                (visorElement as HTMLInputElement).value = '';
-              break;
+  private setToVisor(value: string): void {
+    const visorElement: HTMLElement | null = this.getVisorElement();
 
-              case '=':
-                if((visorElement as HTMLInputElement).value.length == 0) (visorElement as HTMLInputElement).value = '0';
+    // Limpar visor em caso de erro(s) previsto(s)
+    if(value === 'Erro de Syntaxe :/') {
+      this.clearVisor();
+    }
 
-                (visorElement as HTMLInputElement).value = eval((visorElement as HTMLInputElement).value);
-              break;
-            }
-          break;
-          
-          case KeyTypes.operation:
-          case KeyTypes.numeral:
-          case KeyTypes.symbol:
-            let pseudoValue: string = '';
+    (visorElement as HTMLInputElement).value += value;
+  }
 
-            switch(value) {
-              case 'parenthesis_open':
-                pseudoValue = '(';
-              break;
-
-              case 'parenthesis_close':
-                pseudoValue = ')';
-              break;
-
-              default:
-                pseudoValue = value;
-              break;
-            }
-
-            (visorElement as HTMLInputElement).value += pseudoValue;
-          break;
-
-          default:
-            throw(`A ação '${type}' não é reconhecida!`);
-          break;
-        }
-      } else {
-        throw('O visor não foi encontrado!');
-      }
-
-      console.log("Visor:", (visorElement as HTMLInputElement).value);
-    } catch (error) {
-      console.error(error);
+  private getVisorElement(): HTMLInputElement | null {
+    try {
+      return document.getElementById('calculator-visor') as HTMLInputElement;
+    } catch(error: any) {
+      console.error(`O elemento 'visor' não foi encontrado!`);
+      return null;
     }
   }
 }
